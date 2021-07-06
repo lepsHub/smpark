337bda0a1ebea437bbe507c00185bfaf0aa4624a
Luis Enrique Purizaga <luisenriquepurizagasantos@outlook.com>
2021-07-05 22:37:22 -0500

    url image correct

--- lib/src/pages/list_page.dart
+++ lib/src/pages/list_page.dart
@@ -14,54 +14,58 @@ class ListPage extends StatelessWidget {
       child: Scaffold(
         body: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Icon(CupertinoIcons.location_solid),
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Buscar dirección',
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment:,
+              children: [
+                Row(
+                  children: [
+                    Icon(CupertinoIcons.location_solid),
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Buscar dirección',
+                        ),
                       ),
-                    ),
-                  )
-                ],
-              ),
-              const SizedBox(width: 0,height: 10),
-              BlocBuilder<ListCubit, ListState>(
-                builder: (context, state) {
-                  if (state is ListLoadedState) {
-                    return Expanded(
-                      child: ListView.separated(itemBuilder: (_, position) {
-                        ObjectPark item = state.items[position];
-                        return AspectRatio(
-                          aspectRatio: 3/1,
-                          child: Container(
-                            child: Stack(
-                              children: [
-                                ClipRRect(
-                                  borderRadius: BorderRadius.circular(10),
-                                  clipBehavior: Clip.antiAlias,
-                                  child: Image(image: NetworkImage(item.streeImage))
-                                )
-                              ],
-                            ),
-                          ),
-                        );
-                        },
-                        itemCount: state.items.length,
-                        separatorBuilder: (_,__) => const SizedBox(width: 0,height: 10),
-                      ),
-                    );
-                  }
-                  return Center(child:
-                   CircularProgressIndicator()
-                  );
-                },
-              ),
-            ],
+                    )
+                  ],
+                ),
+                const SizedBox(width: 0, height: 10),
+                BlocBuilder<ListCubit, ListState>(
+                  builder: (context, state) {
+                    if (state is ListLoadedState) {
+                      return Expanded(
+                        child: ListView.separated(
+                          itemBuilder: (_, position) {
+                            ObjectPark item = state.items[position];
+                            return AspectRatio(
+                              aspectRatio: 3 / 1,
+                              child: Container(
+                                child: Stack(
+                                  children: [
+                                    ClipRRect(
+                                        borderRadius: BorderRadius.circular(10),
+                                        clipBehavior: Clip.antiAlias,
+                                        child: Image(
+                                            image:
+                                                NetworkImage(item.streeImage)))
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                          itemCount: state.items.length,
+                          separatorBuilder: (_, __) =>
+                              const SizedBox(width: 0, height: 10),
+                        ),
+                      );
+                    }
+                    return Center(child: CircularProgressIndicator());
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
